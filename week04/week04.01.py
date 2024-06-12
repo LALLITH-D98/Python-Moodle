88f4441cb83722668f8ba0b14ab88caa7bc54f4c
LALLITH-D98 <231001098@rajalakshmi.edu.in>
2024-06-12 11:40:36 +0530

    Create week04.01.py

---
+++ week04/week04.01.py
@@ -0,0 +1,15 @@
+                                Factors of a number
+Determine the factors of a number (i.e., all positive integer values that evenly divide into
+a number).
+For example:
+Input
+20
+Result
+20 1 2 4 5 10
+k=int(input())
+l=[]
+for i in range(1,k+1):
+ if(k%i==0):
+ l.append(i)
+for j in l:
+ print(j,end=' ')
